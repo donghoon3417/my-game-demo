05d2ce365a747a06e2546d3aba615e5a8812fba8
donghoon3417 <142172854+donghoon3417@users.noreply.github.com>
2025-07-13 22:10:59 +0900

    Update server.py

--- my-game-demo/server.py
+++ my-game-demo/server.py
@@ -29,4 +29,6 @@ def handle_move(data):
     emit('position', position, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)

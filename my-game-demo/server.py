5d75194b6c7e9a292bd0cc9aabf4c3ad6ecc71ce
donghoon3417 <142172854+donghoon3417@users.noreply.github.com>
2025-07-14 22:20:12 +0900

    Update server.py

--- my-game-demo/server.py
+++ my-game-demo/server.py
@@ -1,7 +1,4 @@
 import os
-import eventlet
-import eventlet.wsgi
-
 from flask import Flask, send_from_directory
 from flask_socketio import SocketIO, emit
 
@@ -18,27 +15,12 @@ def index():
 def serve_file(path):
     return send_from_directory('public', path)
 
-@socketio.on('move')
-def handle_move(data):
-    global position
-    direction = data.get('direction')
-    if direction == 'left':
-        position['x'] -= 10
-    elif direction == 'right':
-        position['x'] += 10
-    elif direction == 'up':
-        position['y'] -= 10
-    elif direction == 'down':
-        position['y'] += 10
-
-    emit('position', position, broadcast=True)
-
 @socketio.on('drag')
 def handle_drag(data):
     global position
     position['x'] = data['x']
     position['y'] = data['y']
-    emit('position', position, broadcast=True)
+    emit('position', position, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

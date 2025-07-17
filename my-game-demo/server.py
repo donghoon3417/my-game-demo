8d82b211f6bdf04a4800f1b5cbb129ef5614f57d
donghoon3417 <142172854+donghoon3417@users.noreply.github.com>
2025-07-17 23:03:45 +0900

    Update server.py

--- my-game-demo/server.py
+++ my-game-demo/server.py
@@ -1,14 +1,14 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-app = Flask(__name__,
-            template_folder='public',  # HTML 파일 경로 지정
-            static_folder='public')    # JS/CSS/이미지 경로 지정
+app = Flask(__name__, template_folder='public', static_folder='public')
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # public/index.html 사용
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render는 환경변수 PORT 사용
+    socketio.run(app, host='0.0.0.0', port=port)

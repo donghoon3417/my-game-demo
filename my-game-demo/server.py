2c6f615c62ae5906fd85cd812d897fb0f5d0d1f3
donghoon3417 <142172854+donghoon3417@users.noreply.github.com>
2025-07-17 23:03:00 +0900

    Update server.py

--- my-game-demo/server.py
+++ my-game-demo/server.py
@@ -1,20 +1,14 @@
-from flask import Flask, render_template, send_from_directory
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder='public',  # HTML 파일 경로 지정
+            static_folder='public')    # JS/CSS/이미지 경로 지정
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/static/<path:path>')
-def static_files(path):
-    return send_from_directory('static', path)
-
-@socketio.on('drag')
-def handle_drag(data):
-    emit('position', data, broadcast=True)
+    return render_template('index.html')  # public/index.html 사용
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
